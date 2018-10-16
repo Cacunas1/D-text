b5c5699727040eb485deea710c2968c3b5753efa
Cristian Andrés Acuña Sepúlveda <c.acuna.sepulveda@gmail.com>
2018-10-16 17:22:45 -0300

    Example network, not needed anymore.

--- code/networkExample.R
+++
@@ -1,9 +0,0 @@
-library(visNetwork)
-
-load(file = "data/viz.RData")
-
-nodes <- data.frame(id = users$id,
-                    label = users$name)
-edges <- data.frame(from = connections$sender,
-                    to = connections$receiver)
-visNetwork(nodes, edges, width = "100%")
